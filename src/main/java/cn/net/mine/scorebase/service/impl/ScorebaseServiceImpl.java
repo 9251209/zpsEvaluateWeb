055654cdab9c70f47ba19096d0270a6fa68eb741
CYN <9251209@qq.cpm>
2018-11-19 10:49:05 +0800

    2018-11-19
    2018-11-19

--- src/main/java/cn/net/mine/scorebase/service/impl/ScorebaseServiceImpl.java
+++ src/main/java/cn/net/mine/scorebase/service/impl/ScorebaseServiceImpl.java
@@ -4,6 +4,7 @@ package cn.net.mine.scorebase.service.impl;
 
 import cn.net.mine.scorebase.dao.ScorebaseDao;
 import cn.net.mine.scorebase.service.ScorebaseService;
+import cn.net.mine.scorezone.dao.ScorezoneDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,8 @@ public class ScorebaseServiceImpl implements ScorebaseService {
 
     @Autowired
     private ScorebaseDao scorebaseDao;
+    @Autowired
+    private ScorezoneDao scorezoneDao;
 
     @Override
     public Object scorebaseAdd(String type, String tablename, String formulaid, String weight, String parama, String paramb, String paramc, String paramd, String paramy, String paramformula, String ua, String ub, String uc, String ud, String uy, String uformula, String explaina, String explainb, String explainc, String explaind, String explainy, String explainformula) {
@@ -49,6 +52,14 @@ public class ScorebaseServiceImpl implements ScorebaseService {
 
     @Override
     public List<Map<String, Object>> selectGetScorebaseList(String type, String tablename, String formulaid, String weight, String parama, String paramb, String paramc, String paramd, String paramy, String paramformula, String ua, String ub, String uc, String ud, String uy, String uformula, String explaina, String explainb, String explainc, String explaind, String explainy, String explainformula) {
-        return this.scorebaseDao.selectGetScorebaseList(type, tablename, formulaid, weight, parama, paramb, paramc, paramd, paramy, paramformula, ua, ub, uc, ud, uy, uformula, explaina, explainb, explainc, explaind, explainy, explainformula);
+        List<Map<String, Object>> scorebaseList= this.scorebaseDao.selectGetScorebaseList(type, tablename, formulaid, weight, parama, paramb, paramc, paramd, paramy, paramformula, ua, ub, uc, ud, uy, uformula, explaina, explainb, explainc, explaind, explainy, explainformula);
+        for (Map<String, Object> scorebase: scorebaseList ){
+            List<Map<String, Object>>  scorezonetList=this.scorezoneDao.selectGetScorezoneList(null, null, null, null, null, null, null, null, null, null, null, null, null, scorebase.get("id").toString());
+            if(scorezonetList.size()>0){
+                scorebase.put("scorezonet", scorezonetList.get(0));
+            }else
+            scorebase.put("scorezonet", null);
+        }
+        return  scorebaseList;
     }
 }
